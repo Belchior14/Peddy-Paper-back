7a3c227cddbf877c55d9ff6fb1eccbbd53f7f5fe
Belchior <belchior.fontao@gmail.com>
2023-11-26 14:20:33 +0000

    cors9

--- config/index.js
+++ config/index.js
@@ -36,7 +36,7 @@ module.exports = (app) => {
 
 
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', "https://hms-front-c6p6jl5is-belchior14s-projects.vercel.app/");
+    res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL);
     // Other CORS headers can be set here.
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
